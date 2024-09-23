b7683e2d26b0c3633dd9a955f72a68f92ea06ce9
chenyang <chenyang.48671155@163.com>
2024-09-23 14:45:07 +0800

    selenium 学习

---
+++ day2/seleniumStudy/seleniumDemo1.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+browser.get("https://www.baidu.com")
+
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+print(browser.title)
+
+browser.quit()
