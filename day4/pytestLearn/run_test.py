c6c76ed3c9ef271bc1082de23d01e0d2f426a112
chenyang <chenyang.48671155@163.com>
2024-09-24 17:06:40 +0800

    pytest 学习

---
+++ day4/pytestLearn/run_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@文件        :Untitled-3
+@说明        :
+@时间        :2024/09/24 17:02:03
+@作者        :chenyang
+@版本        :1.0
+'''
+
+import pytest
+
+pytest.main()
