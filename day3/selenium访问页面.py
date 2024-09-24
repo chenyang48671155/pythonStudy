6bfec572fe791779a24ffa66a8c61ef0e1d2c3c5
chenyang <chenyang.48671155@163.com>
2024-09-24 16:44:01 +0800

    selenium 学习

--- day3/selenium访问页面.py
+++ day3/selenium访问页面.py
@@ -1,3 +1,10 @@
+/*
+ * @Author: UniversalSpaceman-Chenyang 
+ * @Date: 2024-09-24 16:43:23 
+ * @Last Modified by:   UniversalSpaceman-Chenyang 
+ * @Last Modified time: 2024-09-24 16:43:23 
+ */
+
 from selenium import webdriver
 
 browser = webdriver.Chrome()
---
+++ day3/test.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2024-09-24 16:39:43 
+ * @Last Modified by:   mikey.zhaopeng 
+ * @Last Modified time: 2024-09-24 16:39:43 
+ */
